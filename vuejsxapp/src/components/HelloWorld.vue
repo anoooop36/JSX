4e49cc548397455f8b91ca7eecf3707e0261dfdd
Chaudhary, Anoop <anoop.chaudhary@thermofisher.com>
2018-06-25 14:39:45 +0530

    Render function used instead of template

---
+++ vuejsxapp/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  render (createElement) {
+     return (
+         <button domPropsInnerHTML={this.msg} onClick={this.handleClick}></button>
+     )
+ },
+ methods: {
+   handleClick () {
+     alert('button clicked')
+   }
+ }
+}
+</script>
+
